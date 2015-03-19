969bcd05e8cd15059738f0ccf2342bff5fc852d6
Duke Jones <Duke-Jones@users.noreply.github.com>
2015-03-19 21:13:35 +0100

    added some forgotten files for 1.84_0.09_2

---
+++ RegulatedNoise/Ocr/Calibration/FilterTest.Designer.cs
@@ -0,0 +1,277 @@
+﻿    partial class FilterTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FilterTest));
+            this.nudCutoffValue = new System.Windows.Forms.NumericUpDown();
+            this.cmdSaveClose = new System.Windows.Forms.Button();
+            this.cmdClose = new System.Windows.Forms.Button();
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.paPicturePanel = new System.Windows.Forms.Panel();
+            this.pbPicture = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pb_calibratorMagnifier = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pbSampleTooHigh = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pbSampleTooLow = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCutoffValue)).BeginInit();
+            this.paPicturePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_calibratorMagnifier)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSampleTooHigh)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSampleTooLow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudCutoffValue
+            // 
+            this.nudCutoffValue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.nudCutoffValue.Location = new System.Drawing.Point(863, 596);
+            this.nudCutoffValue.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.nudCutoffValue.Name = "nudCutoffValue";
+            this.nudCutoffValue.Size = new System.Drawing.Size(57, 20);
+            this.nudCutoffValue.TabIndex = 1;
+            this.nudCutoffValue.ValueChanged += new System.EventHandler(this.nudCutoffValue_ValueChanged);
+            // 
+            // cmdSaveClose
+            // 
+            this.cmdSaveClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdSaveClose.Location = new System.Drawing.Point(641, 651);
+            this.cmdSaveClose.Name = "cmdSaveClose";
+            this.cmdSaveClose.Size = new System.Drawing.Size(151, 23);
+            this.cmdSaveClose.TabIndex = 2;
+            this.cmdSaveClose.Text = "Save New Value and Close ";
+            this.cmdSaveClose.UseVisualStyleBackColor = true;
+            this.cmdSaveClose.Click += new System.EventHandler(this.cmdSaveClose_Click);
+            // 
+            // cmdClose
+            // 
+            this.cmdClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdClose.Location = new System.Drawing.Point(817, 651);
+            this.cmdClose.Name = "cmdClose";
+            this.cmdClose.Size = new System.Drawing.Size(103, 23);
+            this.cmdClose.TabIndex = 3;
+            this.cmdClose.Text = "Close";
+            this.cmdClose.UseVisualStyleBackColor = true;
+            this.cmdClose.Click += new System.EventHandler(this.cmdCloseOnly_Click);
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel.AutoSize = true;
+            this.flowLayoutPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            this.flowLayoutPanel.Size = new System.Drawing.Size(0, 0);
+            this.flowLayoutPanel.TabIndex = 4;
+            // 
+            // paPicturePanel
+            // 
+            this.paPicturePanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.paPicturePanel.AutoScroll = true;
+            this.paPicturePanel.Controls.Add(this.pbPicture);
+            this.paPicturePanel.Location = new System.Drawing.Point(12, 18);
+            this.paPicturePanel.Name = "paPicturePanel";
+            this.paPicturePanel.Size = new System.Drawing.Size(627, 567);
+            this.paPicturePanel.TabIndex = 5;
+            // 
+            // pbPicture
+            // 
+            this.pbPicture.Location = new System.Drawing.Point(3, 3);
+            this.pbPicture.Name = "pbPicture";
+            this.pbPicture.Size = new System.Drawing.Size(421, 335);
+            this.pbPicture.TabIndex = 0;
+            this.pbPicture.TabStop = false;
+            this.pbPicture.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pbPicture_Click);
+            this.pbPicture.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbPicture_MouseDown);
+            this.pbPicture.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbPicture_MouseMove);
+            this.pbPicture.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbPicture_MouseUp);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.pb_calibratorMagnifier);
+            this.groupBox1.Location = new System.Drawing.Point(653, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(267, 187);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Magnifier (Click On Main Picture)";
+            // 
+            // pb_calibratorMagnifier
+            // 
+            this.pb_calibratorMagnifier.BackColor = System.Drawing.Color.DarkGray;
+            this.pb_calibratorMagnifier.Location = new System.Drawing.Point(6, 19);
+            this.pb_calibratorMagnifier.Name = "pb_calibratorMagnifier";
+            this.pb_calibratorMagnifier.Size = new System.Drawing.Size(255, 162);
+            this.pb_calibratorMagnifier.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_calibratorMagnifier.TabIndex = 0;
+            this.pb_calibratorMagnifier.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.pbSampleTooHigh);
+            this.groupBox2.Location = new System.Drawing.Point(653, 205);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(267, 187);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sample: CutOff-Value Too High";
+            // 
+            // pbSampleTooHigh
+            // 
+            this.pbSampleTooHigh.BackColor = System.Drawing.Color.DarkGray;
+            this.pbSampleTooHigh.Image = ((System.Drawing.Image)(resources.GetObject("pbSampleTooHigh.Image")));
+            this.pbSampleTooHigh.Location = new System.Drawing.Point(6, 19);
+            this.pbSampleTooHigh.Name = "pbSampleTooHigh";
+            this.pbSampleTooHigh.Size = new System.Drawing.Size(255, 162);
+            this.pbSampleTooHigh.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSampleTooHigh.TabIndex = 0;
+            this.pbSampleTooHigh.TabStop = false;
+            this.pbSampleTooHigh.Click += new System.EventHandler(this.pbSampleTooHigh_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.pbSampleTooLow);
+            this.groupBox3.Location = new System.Drawing.Point(653, 398);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(267, 187);
+            this.groupBox3.TabIndex = 17;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Sample: CutOff-Value Too Low";
+            // 
+            // pbSampleTooLow
+            // 
+            this.pbSampleTooLow.BackColor = System.Drawing.Color.DarkGray;
+            this.pbSampleTooLow.Image = ((System.Drawing.Image)(resources.GetObject("pbSampleTooLow.Image")));
+            this.pbSampleTooLow.Location = new System.Drawing.Point(6, 19);
+            this.pbSampleTooLow.Name = "pbSampleTooLow";
+            this.pbSampleTooLow.Size = new System.Drawing.Size(255, 162);
+            this.pbSampleTooLow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSampleTooLow.TabIndex = 0;
+            this.pbSampleTooLow.TabStop = false;
+            this.pbSampleTooLow.Click += new System.EventHandler(this.pbSampleTooLow_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 601);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(644, 78);
+            this.label1.TabIndex = 18;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(693, 598);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "UI Color Cutoff Level (0 to 255)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(681, 614);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "(Activate A New Value With [Enter])";
+            // 
+            // FilterTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(932, 686);
+            this.Controls.Add(this.cmdSaveClose);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.paPicturePanel);
+            this.Controls.Add(this.flowLayoutPanel);
+            this.Controls.Add(this.cmdClose);
+            this.Controls.Add(this.nudCutoffValue);
+            this.Name = "FilterTest";
+            this.Text = "FilterTest";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FilterTest_FormClosing);
+            this.Load += new System.EventHandler(this.FilterTest_Load);
+            this.Shown += new System.EventHandler(this.FilterTest_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.nudCutoffValue)).EndInit();
+            this.paPicturePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_calibratorMagnifier)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSampleTooHigh)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSampleTooLow)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudCutoffValue;
+        private System.Windows.Forms.Button cmdSaveClose;
+        private System.Windows.Forms.Button cmdClose;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+        private System.Windows.Forms.Panel paPicturePanel;
+        private System.Windows.Forms.PictureBox pbPicture;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pb_calibratorMagnifier;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pbSampleTooHigh;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox pbSampleTooLow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
---
+++ RegulatedNoise/Ocr/Calibration/FilterTest.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RegulatedNoise.Enums_and_Utility_Classes;
+
+public partial class FilterTest : Form
+{
+    private Bitmap _TestBitmap;
+    private Rectangle _MagnifierPosition;
+    private bool _MousebuttonIsDown = false;
+    
+    public FilterTest()
+    {
+        InitializeComponent();
+    }
+
+    public int CutoffLevel { get; set; }
+
+    public Bitmap TestBitmap
+    {
+        get
+        {
+            return _TestBitmap;
+        }
+        set
+        {
+            _TestBitmap = value;
+        }
+    }
+
+    private void FilterTest_Load(object sender, EventArgs e)
+    {
+        Cursor = Cursors.WaitCursor;
+        this.DialogResult = System.Windows.Forms.DialogResult.None;
+        
+        nudCutoffValue.Value = CutoffLevel;
+
+        pbPicture.Image = RNGraphics.PreprocessScreenshot(_TestBitmap, 1, (int)(nudCutoffValue.Value));
+
+        pbPicture.Size = pbPicture.Image.Size;
+        Cursor = Cursors.Default;
+    }
+
+    private void cmdCloseOnly_Click(object sender, EventArgs e)
+    {
+        this.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        this.Close();
+    }
+
+    private void cmdSaveClose_Click(object sender, EventArgs e)
+    {
+        this.DialogResult = System.Windows.Forms.DialogResult.OK;
+        this.Close();
+    }
+
+    private void FilterTest_Shown(object sender, EventArgs e)
+    {
+        Cursor = Cursors.WaitCursor;
+        _MagnifierPosition = new Rectangle((paPicturePanel.Width / 2) - 25, (paPicturePanel.Height / 2) - 17, 50, 31); 
+        
+        setMagnifier();
+        Cursor = Cursors.Default;
+    }
+
+    private void setMagnifier()
+    {
+        if (pb_calibratorMagnifier.Image != null)
+            pb_calibratorMagnifier.Image.Dispose();
+
+        if (_MagnifierPosition.Width>0 && _MagnifierPosition.Height > 0)
+            pb_calibratorMagnifier.Image = RNGraphics.Crop((Bitmap)(pbPicture.Image), _MagnifierPosition);
+    }
+
+    private void FilterTest_FormClosing(object sender, System.Windows.Forms.FormClosingEventArgs e)
+    {
+        Cursor = Cursors.WaitCursor;
+
+        if (this.DialogResult == System.Windows.Forms.DialogResult.None)
+        {
+            this.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        }
+        CutoffLevel = (int)(nudCutoffValue.Value);
+        Cursor = Cursors.Default;
+    }
+
+    private void nudCutoffValue_ValueChanged(object sender, EventArgs e)
+    {
+        Cursor = Cursors.WaitCursor;
+        pbPicture.Image = RNGraphics.PreprocessScreenshot(_TestBitmap, 1, (int)(nudCutoffValue.Value));    
+        setMagnifier();
+        Cursor = Cursors.Default;
+    }
+
+    private void pbPicture_Click(object sender, MouseEventArgs e)
+    {
+        Cursor = Cursors.WaitCursor;
+        _MagnifierPosition = new Rectangle(e.X - 25, e.Y - 17, 50, 31);
+        setMagnifier();
+        Cursor = Cursors.Default;
+    }
+
+    private void pbSampleTooHigh_Click(object sender, EventArgs e)
+    {
+        //pb_calibratorMagnifier.Image.Save(@"C:\temp\SampleTooHigh.png");
+    }
+
+    private void pbSampleTooLow_Click(object sender, EventArgs e)
+    {
+        //pb_calibratorMagnifier.Image.Save(@"C:\temp\SampleTooLow.png");
+    }
+
+    private void pbPicture_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (_MousebuttonIsDown)
+        {
+            _MagnifierPosition = new Rectangle(e.X - 25, e.Y - 17, 50, 31);
+            setMagnifier();
+        }
+
+    }
+
+    private void pbPicture_MouseUp(object sender, MouseEventArgs e)
+    {
+        _MousebuttonIsDown = false;
+    }
+
+    private void pbPicture_MouseDown(object sender, MouseEventArgs e)
+    {
+        _MousebuttonIsDown = true;
+    }
+
+   
+}
