5ce6352d1fa530699fd644dd58ec8ce1deba99c0
Duke Jones <Duke-Jones@users.noreply.github.com>
2015-04-08 09:40:06 +0200

    * added missing ProgressBar

---
+++ RegulatedNoise/ProgressView.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace RegulatedNoise
+{
+    partial class ProgressView
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.lblInfotext = new System.Windows.Forms.Label();
+            this.cmdCancel = new System.Windows.Forms.Button();
+            this.ProgressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoEllipsis = true;
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.BackColor = System.Drawing.Color.Transparent;
+            this.lblProgress.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProgress.Location = new System.Drawing.Point(189, 84);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(34, 13);
+            this.lblProgress.TabIndex = 10;
+            this.lblProgress.Text = "33 %";
+            this.lblProgress.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblInfotext
+            // 
+            this.lblInfotext.AutoEllipsis = true;
+            this.lblInfotext.BackColor = System.Drawing.Color.Transparent;
+            this.lblInfotext.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.lblInfotext.Location = new System.Drawing.Point(12, 9);
+            this.lblInfotext.Name = "lblInfotext";
+            this.lblInfotext.Size = new System.Drawing.Size(389, 46);
+            this.lblInfotext.TabIndex = 9;
+            this.lblInfotext.Text = "Info";
+            this.lblInfotext.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cmdCancel
+            // 
+            this.cmdCancel.Location = new System.Drawing.Point(147, 100);
+            this.cmdCancel.Name = "cmdCancel";
+            this.cmdCancel.Size = new System.Drawing.Size(118, 31);
+            this.cmdCancel.TabIndex = 8;
+            this.cmdCancel.Text = "Cancel";
+            this.cmdCancel.UseVisualStyleBackColor = true;
+            this.cmdCancel.Click += new System.EventHandler(this.cmdCancel_Click);
+            // 
+            // ProgressBar1
+            // 
+            this.ProgressBar1.Location = new System.Drawing.Point(12, 59);
+            this.ProgressBar1.Name = "ProgressBar1";
+            this.ProgressBar1.Size = new System.Drawing.Size(389, 22);
+            this.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.ProgressBar1.TabIndex = 7;
+            this.ProgressBar1.Value = 55;
+            // 
+            // ProgressView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(414, 141);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.lblInfotext);
+            this.Controls.Add(this.cmdCancel);
+            this.Controls.Add(this.ProgressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(430, 179);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(430, 179);
+            this.Name = "ProgressView";
+            this.Text = "Progress";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label lblProgress;
+        internal System.Windows.Forms.Label lblInfotext;
+        internal System.Windows.Forms.Button cmdCancel;
+        internal System.Windows.Forms.ProgressBar ProgressBar1;
+    }
+}
---
+++ RegulatedNoise/ProgressView.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RegulatedNoise
+{
+    public partial class ProgressView : RegulatedNoise.Enums_and_Utility_Classes.RNBaseForm
+    {
+        PerformanceTimer m_PTimer = new PerformanceTimer();
+        //string THISOBJECTNAME = "FormProgressView";
+        public delegate void del_Sub_PInt32(Int32 Value);
+        public delegate void del_Sub_PString(string Text);
+        bool m_Cancelled;           //  True: Abbruch wurde angefordert
+
+        public ProgressView()
+        {
+            InitializeComponent();
+
+            m_Cancelled = false;
+            m_PTimer.startMeasuring();
+        }
+
+
+    
+    // <summary>
+    // shows the progress view
+    // </summary>
+    // <param name="Info"></param>
+    // <remarks></remarks>
+    public void progressStart(string Info="") {
+        try {
+            this.Show();
+            progressUpdate(0);
+            progressInfo(Info);
+            Application.DoEvents();
+            this.TopMost = true;
+        }
+        catch (Exception ex) {
+            throw new Exception("error while starting the progress view", ex);
+        }
+    }
+    
+    // <summary>
+    // set the progress to a new value
+    // </summary>
+    // <param name="Value">progress current value</param>
+    // <param name="Total">total value</param>
+    // <remarks></remarks>
+    public void progressUpdate(Int32 Value, Int32 Total) {
+        Int32 ProzValue;
+        try {
+            if ((Total > 0)) {
+                ProzValue = (int)Math.Round((double)(Value) / (double)(Total) * 100.0, 0);
+
+                if ((ProzValue < 0)) {
+                    ProzValue = 0;
+                }
+                else if ((ProzValue > 100)) {
+                    ProzValue = 100;
+                }
+            }
+            else {
+                ProzValue = 0;
+            }
+            if (m_PTimer.currentMeasuring() >= 100)
+            { 
+                m_PTimer.startMeasuring();
+                progressUpdate(ProzValue);
+            }
+            
+        }
+        catch (Exception ex) {
+            throw new Exception("error while setting a new value", ex);
+        }
+    }
+    
+    // <summary>
+    // set the progress to a new value
+    // </summary>
+    // <param name="Percent">progress in percent</param>
+    // <remarks></remarks>
+    private void progressUpdate(Int32 Percent) {
+        try {
+            if (ProgressBar1.InvokeRequired) {
+                ProgressBar1.Invoke(new del_Sub_PInt32(this.progressUpdate), Percent);
+            }
+            else {
+                ProgressBar1.Value = Percent;
+                if (Percent>0 && Percent<100)
+                    ProgressBar1.Value = Percent-1;
+
+                lblProgress.Text = string.Format("{0}%", Percent);
+                ProgressBar1.Refresh();
+                Application.DoEvents();
+            }
+        }
+        catch (Exception ex) {
+            throw new Exception("error while setting a new value", ex);
+        }
+    }
+    
+    // <summary>
+    // sets a new info string
+    // </summary>
+    // <param name="Info">new information</param>
+    // <remarks></remarks>
+    public void progressInfo(string Info) {
+        try {
+            if (lblInfotext.InvokeRequired) {
+                lblInfotext.Invoke(new del_Sub_PString(this.progressInfo), Info);
+            }
+            else {
+                if (string.IsNullOrEmpty(Info)) {
+                    this.Height = 125;
+                }
+                else {
+                    this.Height = 161;
+                }
+                lblInfotext.Text = Info;
+                Application.DoEvents();
+            }
+        }
+        catch (Exception ex) {
+            throw new Exception("error while setting a newinfo string", ex);
+        }
+    }
+    
+    // <summary>
+    // closes the progress view
+    // </summary>
+    // <remarks></remarks>
+    public void progressStop() {
+        try {
+            this.Close();
+        }
+        catch (Exception ex) {
+            throw new Exception("error while closing the progress view", ex);
+        }
+    }
+    
+    // <summary>
+    // show if the user has "cancel" clicked
+    // </summary>
+    // <value></value>
+    // <returns></returns>
+    // <remarks></remarks>
+    public bool Cancelled {
+        get {
+            return m_Cancelled;
+        }
+    }
+    
+    /// <summary>
+    /// event function of cancel button
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void cmdCancel_Click(object sender, System.EventArgs e) {
+        m_Cancelled = true;
+    }
+
+}
+}
